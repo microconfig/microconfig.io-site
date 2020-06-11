f804b4675ac42e017fcbd83ec4886f24c1bb45c6
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-06-11 23:28:28 +0300

    Added height detection with two types switchers

--- script/setCodeBlockHeight.js
+++ script/setCodeBlockHeight.js
@@ -1,13 +1,7 @@
 
-const main = document.querySelector('.main');
-
 function isCheckbox(element) {
     return element instanceof HTMLInputElement
         && element.getAttribute('type') == 'checkbox'
-}
-
-function hideBlockHandler(event) {
-    console.log('hhhh', event.target)
 }
 
 function getExampleBlockHeight(width, element) {
@@ -28,21 +22,14 @@ function getExampleBlockHeight(width, element) {
         const rowPadds = [];
 
         codeRows.forEach(row => {
+
             let codeTitles = Array.from(row.querySelectorAll('.code-title'));
             let codeBlocks = Array.from(row.querySelectorAll('.code-block'));
 
-            console.log(Array.from(row.children))
-            console.log(codeBlocks.length)
-
             codeBlocks.map((block, i) => {
-                // if (codeBlocks.lenght % 2 == 0) {
                 if (block.closest('div').classList.contains('common')) {
                     commonRowHeights.push(outerHeight(block) + outerHeight(codeTitles[i]));
                 }
-                // }
-                // else {
-                //     element.addEventListener('click', hideBlockHandler)
-                // }
 
                 if (block.closest('div').classList.contains('off')) {
                     offRowHeights.push(outerHeight(block) + outerHeight(codeTitles[i]));
@@ -54,26 +41,29 @@ function getExampleBlockHeight(width, element) {
                 }
             });
 
-            if (commonRowHeights.length != 0) { commonHeights.push(Math.max(...commonRowHeights)); }
-            if (offRowHeights.length != 0) { offHeights.push(Math.max(...offRowHeights)); }
-            if (onRowHeights.length != 0) { onHeights.push(Math.max(...onRowHeights)); }
+            if (width >= 768) {
+                if (commonRowHeights.length != 0) { commonHeights.push(Math.max(...commonRowHeights)); }
+                if (offRowHeights.length != 0) { offHeights.push(Math.max(...offRowHeights)); }
+                if (onRowHeights.length != 0) { onHeights.push(Math.max(...onRowHeights)); }
+            }
+
+            else {
+                if (commonRowHeights.length != 0) { commonHeights.push(...commonRowHeights); }
+                if (offRowHeights.length != 0) { offHeights.push(...offRowHeights); }
+                if (onRowHeights.length != 0) { onHeights.push(...onRowHeights); }
+            }
+
             rowPadds.push(parseInt(getComputedStyle(row).paddingTop));
 
             commonRowHeights = [];
             offRowHeights = [];
             onRowHeights = [];
-
         });
 
         const switcherHeight = outerHeight(label[0]);
-
         const rowsPaddings = sumArray(rowPadds);
 
         if (width >= 768) {
-            // const commonRowHeights = [...new Set(commonHeights)];
-            // const offRowHeights = [...new Set(offHeights)];
-            // const onRowHeights = [...new Set(onHeights)];
-
             setHeights([...new Set(commonHeights)], [...new Set(offHeights)], [...new Set(onHeights)]);
         }
 
@@ -117,23 +107,7 @@ document.body.onload = function () {
         || document.documentElement.clientWidth
         || document.body.clientWidth;
 
-    console.log(screenWidth)
-
     const switchersBlocks = Array.from(document.querySelectorAll('.toggle'));
 
-    // switchersBlocks.forEach(block => getExampleBlockHeight(screenWidth, block));
-
-    const incl = document.getElementById('toggle-incl');
-    const ref = document.getElementById('toggle-ref');
-    const expr = document.getElementById('toggle-expr');
-    const env = document.getElementById('toggle-env');
-    const tmpl = document.getElementById('toggle-tmpl');
-    const diff = document.getElementById('toggle-diff');
-
-    getExampleBlockHeight(screenWidth, incl);
-    getExampleBlockHeight(screenWidth, ref);
-    getExampleBlockHeight(screenWidth, expr);
-    getExampleBlockHeight(screenWidth, env);
-    getExampleBlockHeight(screenWidth, tmpl);
-    getExampleBlockHeight(screenWidth, diff);
+    switchersBlocks.forEach(block => getExampleBlockHeight(screenWidth, block));
 };
